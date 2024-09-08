663274a18ca859b0076f401ea997c6fee65a3c37
omrawat23 <omrawat23@gmail.com>
2024-09-08 12:51:31 +0530

    secret key updated

--- lib/pineconeClient.ts
+++ lib/pineconeClient.ts
@@ -1,7 +1,6 @@
 import { Pinecone } from '@pinecone-database/pinecone';
 
 export const pc = new Pinecone({
-  apiKey: process.env.PINECONE_API_KEY
+  apiKey: process.env.PINECONE_API_KEY!,
 });
-
 export const index = pc.index('talktocode');
